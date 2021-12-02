f6fbfa70f2d5ce2f51709ad2c77e517e28501972
Jay <jay.lee.2280@gmail.com>
2021-12-02 22:00:38 +0900

    Use Route for paging

--- my-movies/src/App.js
+++ my-movies/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
 import { HashRouter, Route } from 'react-router-dom';
-
 import Navbar from './components/Navbar';
+import Home from './routes/Home';
+import Detail from './routes/Detail';
 
 function App() {
   return (
     <HashRouter>
       <Navbar></Navbar>
+      {/* <Route path="/" exact component={Home}></Route> */}
+      <Route path="/detail" component={Detail}></Route>
     </HashRouter>
   );
 }
---
+++ my-movies/src/routes/Detail.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Detail.css';
+
+function Detail() {
+  return <div className="detail">Detail Page</div>;
+}
+
+export default Detail;
