a13ef7e2440f02be414e6f951c16afa9ef77695f
Jay <jay.lee.2280@gmail.com>
2021-12-11 11:13:46 +0900

    Add Ant Design to Search bar

--- my-movies/src/pages/home/Searchbar.js
+++ my-movies/src/pages/home/Searchbar.js
@@ -1,4 +1,7 @@
 import React, { useRef } from 'react';
+import 'antd/dist/antd.css';
+import { Input, Button, Select, Tooltip } from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
 
 function Searchbar() {
   const inputRef = useRef();
@@ -17,18 +20,39 @@ function Searchbar() {
     handleSearch();
   };
 
+  const { Option } = Select;
+
   return (
     <div>
-      <div>Genre</div>
       <input
         ref={inputRef}
         type="search"
         placeholder="Search..."
         onKeyPress={onKeyPress}
       />
-      <button type="submit" onClick={onClick}>
-        Search
-      </button>
+      <Input.Group compact>
+        <Select defaultValue="Genre" style={{ width: '12%' }}>
+          <Option value="Action">Action</Option>
+          <Option value="Comedy">Comedy</Option>
+          <Option value="Documentary">Documentary</Option>
+          <Option value="Horror">Horror</Option>
+        </Select>
+        <Input
+          style={{ width: '50%' }}
+          placeholder="Search..."
+          ref={inputRef}
+          onKeyPress={onKeyPress}
+          type="search"
+        />
+      </Input.Group>
+      <Tooltip title="search">
+        <Button
+          shape="circle"
+          icon={<SearchOutlined />}
+          type="submit"
+          onClick={onClick}
+        />
+      </Tooltip>
     </div>
   );
 }
