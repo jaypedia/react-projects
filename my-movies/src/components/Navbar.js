5c2d3ea55db05103cfe5c66f8abfc584ae65c3d2
Jay <jay.lee.2280@gmail.com>
2021-12-28 15:30:23 +0900

    Add logo to navigation bar

--- my-movies/src/components/Navbar.js
+++ my-movies/src/components/Navbar.js
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
+import { Menu } from 'antd';
+import { VideoCameraOutlined } from '@ant-design/icons';
 
 function Navbar() {
+  const [current, setCurrent] = useState('my-movies');
+
+  const handleClick = e => {
+    setCurrent(e.key);
+  };
+
   return (
-    <div className="navbar">
-      <Link to="/" className="navbar__home">
-        My Movies
-      </Link>
-    </div>
+    <Menu
+      className="navbar"
+      onClick={handleClick}
+      selectedKeys={[current]}
+      mode="horizontal"
+      style={{
+        backgroundColor: '#c5d2ec',
+        height: '70px',
+        fontSize: '20px',
+        fontWeight: 'bold',
+        lineHeight: '70px',
+      }}
+    >
+      <Menu.Item key="logo">
+        <VideoCameraOutlined style={{ fontSize: '25px' }} />
+      </Menu.Item>
+
+      <Menu.Item key="my-movies">
+        <Link to="/" className="navbar__home">
+          My Movies
+        </Link>
+      </Menu.Item>
+    </Menu>
   );
 }
 
